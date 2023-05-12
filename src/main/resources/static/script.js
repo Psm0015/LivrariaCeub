cbc2417c5c1cbbae76ab700646941063d0e2999a
Psm0015 <pedro.mota@df.estudante.senai.br>
2023-05-12 17:22:18 -0300

    Ta pronto

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,4 +1,7 @@
 carregatabela()
+
+//SCRIPT DE CARREGAR A TABELA
+
 function carregatabela() {
     $.ajax({
         url: "livros",
@@ -7,14 +10,18 @@ function carregatabela() {
             tabela = ''
             for (let i = 0; i < obj.length; i++) {
                 const livro = obj[i];
-                tabela+=`<tr><td scope="col">${livro.titulo}</td><td scope="col">${livro.autor}</td><td><i class="fa fa-pencil-square-o" aria-hidden="true"></i></td></tr>`
+                tabela+=`<tr><td id="l${livro.id}" scope="col">${livro.titulo}</td><td id="a${livro.id}" scope="col">${livro.autor}</td><td>   <i  style="cursor:pointer ;" class="fa-solid fa-pen-to-square" onclick="abremodaleditar(${livro.id})"></i>   <i style="cursor:pointer ;color: red;" class="fa-solid fa-trash" onclick="apagarlivro(${livro.id})"></i></td></tr>`
             }
             $('#tabela').html(tabela)
         }
     });
 }
+
+// SCRIPT DE CADASTRO
+
 function abremodalcadastro() {
     $("#modalForm").modal('show');
+
 }
 function enviarcadastro() {
     titulo = $('#titulo').val()
@@ -29,6 +36,46 @@ function enviarcadastro() {
             $("#modalForm").modal('hide');
             $('#titulo').val('')
             $('#autor').val('')
+            carregatabela()
+        }
+    });
+}
+
+// SCRIPT DE EDIÇÃO
+
+function abremodaleditar(id) {
+    $("#modalcad").modal('show');
+    livro=document.getElementById('l'+id).innerHTML
+    autor=document.getElementById('a'+id).innerHTML
+    titulo = $('#tituloed').val(livro)
+    autor = $('#autored').val(autor)
+    sessionStorage.setItem('id',id)
+}
+
+function editarcadastro() {
+    titulo = $('#tituloed').val()
+    autor = $('#autored').val()
+
+    $.ajax({
+        url: "livros/"+sessionStorage.getItem('id'),
+        type: "PUT",
+        data: `{\n  \"titulo\": \"${titulo}\",\n  \"autor\": \"${autor}\"\n}`,
+        contentType: "application/json",
+        success: function(response) {
+            $("#modalcad").modal('hide');
+            $('#tituloed').val('')
+            $('#autored').val('')
+            carregatabela()
+        }
+    });
+
+}
+
+function apagarlivro(id) {
+    $.ajax({
+        url: "livros/"+id,
+        type: "DELETE",
+        success: function(response) {
             carregatabela()
         }
     });
